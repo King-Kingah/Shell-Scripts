92199b6a0d06f2e9debcb3d78a80dca317545f58
King-Kingah <kingajames2015@gmail.com>
2023-12-02 13:50:54 +0300

    Remove previous file.

--- zsh-corrupt-history.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-#
-# Credit to George Ornbo (https://shapeshed.com/zsh-corrupt-history-file/)
-# License - http://unlicense.org
-#
-#
-# A script to fix a corrupt .zsh_history file.
-
-mv ~/.zsh_history ~/.zsh_history_bad
